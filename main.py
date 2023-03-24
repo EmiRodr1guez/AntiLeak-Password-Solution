b7da75e580feb4acb7dae93ae5d60d277a57a6cb
IDFKEMI <IDFKEMI@GMAIL.COM>
2023-03-24 10:47:41 -0600

    Formatted Python main.py code

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def randomdigitPass(length):
     result_str = ''.join(random.choice(characters) for i in range(length))
     return result_str
 
+
 # Initial Sequence
 print("Did you know that 20% of passwords are compromised?\n")
 sleep(1)
@@ -36,13 +37,14 @@ sleep(2)
 userPass = stdiomask.getpass(prompt="Please enter your current password : ")
 
 # API Key
-api_key = '*******************' # < ******* Please enter your own API key. *******
+api_key = '*******************'  # < ******* Please enter your own API key. *******
 count = checkLeak(userPass, api_key)
 
 if count <= 0:
     print("Your password has not been compromised. Pat yourself on the back\n")
     sleep(1)
-    print("That being said, true security matters. Hence why you should come back periodically to ensure your passwords are safe\n")
+    print(
+        "That being said, true security matters. Hence why you should come back periodically to ensure your passwords are safe\n")
     sleep(1)
     print(Fore.CYAN + "Take care, and much ❣️. -Emiliano R.")
 
@@ -54,7 +56,7 @@ else:
     print(f"Your password has been compromised {count} times. You should change it ASAP.\n")
     sleep(2)
     print("Dont worry. We generated a couple passwords for you that are similar to your current password. Just so you "
-    "wont forget.\n")
+          "wont forget.\n")
     sleep(2)
     print("These passwords are in ascending order or least complex to most complex\n")
     sleep(2)
@@ -68,30 +70,42 @@ else:
     sleep(2)
 
     print("At the end of the password, the decimal is the strength of the password. anything above .66 is considered a "
-    "STRONG password\n")
+          "STRONG password\n")
     sleep(1)
     print("I recommend always opting for a password that has a score above .66\n")
     sleep(1)
     stats = PasswordStats(userPass + password1)
-    print(Fore.RED + f"Password 1: {userPass + password1} // Strength of this password is => {round(stats.strength(), 2)}")
+    print(
+        Fore.RED + f"Password 1: {userPass + password1} // Strength of this password is => {round(stats.strength(), 2)}")
     sleep(1)
     stats = PasswordStats(userPass + password2)
-    print(Fore.BLUE + f"Password 2: {userPass + password2} // Strength of this password is => {round(stats.strength(), 2)}")
+    print(
+        Fore.BLUE + f"Password 2: {userPass + password2} // Strength of this password is => {round(stats.strength(), 2)}")
     sleep(1)
     stats = PasswordStats(userPass + password3)
-    print(Fore.GREEN + f"Password 3: {userPass + password3} // Strength of this password is => {round(stats.strength(), 2)}")
+    print(
+        Fore.GREEN + f"Password 3: {userPass + password3} // Strength of this password is => {round(stats.strength(), 2)}")
     sleep(1)
     stats = PasswordStats(userPass + password4)
-    print(Fore.CYAN + f"Password 4: {userPass + password4} // Strength of this password is => {round(stats.strength(), 2)}")
+    print(
+        Fore.CYAN + f"Password 4: {userPass + password4} // Strength of this password is => {round(stats.strength(), 2)}")
     saveToFile = bool(input("Would you like to save these passwords to a file? (y/n): "))
 
     while saveToFile:
         with open('UnleakedPass.txt', 'w') as f:
             sleep(1)
-            print(Fore.RED + f"Password 1: {userPass + password1} // Strength of this password is => {round(stats.strength(), 2)}", file=f)
-            print(Fore.BLUE + f"Password 2: {userPass + password2} // Strength of this password is => {round(stats.strength(), 2)}", file=f)
-            print(Fore.GREEN + f"Password 3: {userPass + password3} // Strength of this password is => {round(stats.strength(), 2)}", file=f)
-            print(Fore.CYAN + f"Password 4: {userPass + password4} // Strength of this password is => {round(stats.strength(), 2)}", file=f)
+            print(
+                Fore.RED + f"Password 1: {userPass + password1} // Strength of this password is => {round(stats.strength(), 2)}",
+                file=f)
+            print(
+                Fore.BLUE + f"Password 2: {userPass + password2} // Strength of this password is => {round(stats.strength(), 2)}",
+                file=f)
+            print(
+                Fore.GREEN + f"Password 3: {userPass + password3} // Strength of this password is => {round(stats.strength(), 2)}",
+                file=f)
+            print(
+                Fore.CYAN + f"Password 4: {userPass + password4} // Strength of this password is => {round(stats.strength(), 2)}",
+                file=f)
             sleep(2)
         print('Passwords have been printed to UnleakedPass.txt\n')
         break
@@ -99,6 +113,7 @@ else:
     else:
         print('No worries. You can always restart the program if you wish to generate new passwords.\n')
         sleep(1)
-        print("That being said, true security matters. Hence why you should come back periodically to ensure your passwords are safe\n")
+        print(
+            "That being said, true security matters. Hence why you should come back periodically to ensure your passwords are safe\n")
         sleep(1)
         print(Fore.CYAN + "Take care, and much ❣️. -Emiliano R.")
